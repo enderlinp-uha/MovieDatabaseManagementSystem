3ca5656a643bd49d95fb36b989462de19815af7a
enderlinp-uha <philippe.enderlin@uha.fr>
2025-02-02 21:29:01 +0100

    src/Database.js

--- src/Database.java
+++ src/Database.java
@@ -6,4 +6,12 @@ public class Database {
     public Database() {
         SLogger.getIntance().log(ELogType.INFO, "Database initialized");
     }
+
+    public Movie create(Movie movie) {
+        this.movies.put(movie.getId(), movie);
+
+        SLogger.getIntance().log(ELogType.INFO, "Movie added to Database: " + movie.getTitle());
+
+        return movie;
+    }
 }
